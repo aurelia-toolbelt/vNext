436e8d27e38bcde44f5e5bae5691e4bb995baa32
hamedfathi <noeffect@outlook.com>
2020-05-31 12:23:20 +0430

    chore(bootstrap-v4)

--- packages/bootstrap-v4/index.ts
+++ packages/bootstrap-v4/index.ts
@@ -1,10 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BootstrapConfig } from '@aurelia-toolbelt/bootstrap-core';
+import { BootstrapV4Configuration } from '@aurelia-toolbelt/bootstrap-core';
 import Aurelia, { JitHtmlBrowserConfiguration } from 'aurelia';
 // import 'jquery/dist/jquery.js';
 // import 'bootstrap/dist/js/bootstrap.bundle.js';
-
-Aurelia.register(BootstrapConfig.customize(), JitHtmlBrowserConfiguration);
-
-
+Aurelia.register(BootstrapV4Configuration.customize(), JitHtmlBrowserConfiguration);
 export * from './src';
