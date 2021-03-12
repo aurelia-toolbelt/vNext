5605abb5f3566f0fe805b6a8a6fb228da143641c
HamedFathi <noeffect@outlook.com>
2021-03-12 10:58:36 +0330

    chore: update truthyDetector

--- packages/core/src/utilities.ts
+++ packages/core/src/utilities.ts
@@ -1,12 +1,3 @@
 export function truthyDetector(value: unknown) {
-    const isBoolean =
-        value === true ||
-        value === false ||
-        Object.prototype.toString.call(value) === "[object Boolean]";
-    const isString = Object.prototype.toString.call(value) === "[object String]";
-    if (!isBoolean && !isString) {
-        throw "The custom attribute's type must be 'boolean' or 'string'.";
-    } else {
-        return value === '' || value === true || value === "true";
-    }
+    return value === '' || value === true || value === "true";
 }
